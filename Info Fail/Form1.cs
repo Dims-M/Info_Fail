49a6f9e15afd77414009ca22d67c564901dfcc66
Dims_M <dimsxm@yandex.ru>
2019-01-08 21:56:03 +0300

    Почти финал

--- Info Fail/Form1.cs
+++ Info Fail/Form1.cs
@@ -20,11 +20,30 @@ namespace Info_Fail
             InitializeComponent();
         }
 
+        //МЕТоДЫ
+
+            public String BytestoString(long byteCount)
+        {
+            string[] suf = { "Byt", "Kb", "MB", "GB", "TB", "RB", "EB", };
+            // проверка
+            if (byteCount == 0)
+            {
+                return "0+" + suf[0];
+            }
+
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes,1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+        
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
 
+ 
         // кнопка открыть файл
         private void button1_Click(object sender, EventArgs e)
         {
@@ -37,32 +56,78 @@ namespace Info_Fail
             // переменная для хранения пути
             string FailPath = null;
 
-            // переменная для хранения имени иконки
+            int i = 0;
+
+            // переменная для хранения пути хранения имени иконки
+
             string DirPath = @"tempFile\\Icon\\";
-            
+
+            // переменная для хранения имени иконки и разширения
+            string nameIcon = ofd.SafeFileName;
+
+
             // если при открытой форме опен диалог нажата кнопка Ок
-            if(ofd.ShowDialog() == DialogResult.OK)
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
-                // в переменную текст бокса записываем путь к файлу
-                // запись путей имен файлов в лог
+                // запись путей имен файлов в переменную
                 FailPath = textBox1.Text = ofd.FileName;
 
-                string nameIcon = ofd.SafeFileName;
-
+                // запись имени и путей файлог в лог
                 myUtiliti.ZapisTextFail(FailPath);
 
-                
+                string nameIcon1 = $"{nameIcon}+{i}+.png";
 
-                // сохранение иконки
-                using (var icon = Icon.ExtractAssociatedIcon(FailPath));
+                // путь имя и разширение
+                string tempname = DirPath + nameIcon1;
 
-               // DirPath+ nameIcon
-             //  string d = DirPath + nameIcon;
-                // сохранение файла
-                using (var file = File.Create(DirPath + nameIcon))
-                    Icon.Save(file);
-            }
-           
+                // ЦИКЛ сохранений
+               
+
+                while (true)
+                {
+
+
+                    try
+                    {
+
+                   
+                    // сохранение иконки
+                    using (var icon = Icon.ExtractAssociatedIcon(FailPath))
+
+                    using (var file = File.Create(tempname))
+                        icon.Save(file);
+
+                    var files = new FileInfo(FailPath);
+                    // long size = files.Length;
+
+                    //имя файла. без пути
+                    string namelogg = ofd.SafeFileName;
+
+                    // переменная для хранения размера иконки
+                    string size = BytestoString(files.Length).ToString();
+
+                    // запысываем полученные данные в  датагрид(Таблица)
+
+                    // dataGridView1[0,0].Value = new Bitmap(tempname);
+                    //  dataGridView1[1,0].Value = namelogg;
+                    // dataGridView1[2,0].Value = size;
+
+                    //создаем в гриде новую строку. При каждом  нажатии
+                    dataGridView1.Rows.Add(new Bitmap(tempname), namelogg, size);
+
+                        ///вывод для теста
+                        // MessageBox.Show(ofd.SafeFileName +" "+BytestoString(files.Length));
+
+                        break;
+                    }
+
+                    catch(Exception ex)
+                    {
+                        i++;
+                    }
+
+               }
+            } 
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
